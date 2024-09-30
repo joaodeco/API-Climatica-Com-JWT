dcfae22dcc923e23f9759dc2102b447a7cd41d99
joaodeco <murilomergner@gmail.com>
2024-09-30 16:45:43 -0300

    Instalando bibliotecas necessarias

---
+++ auth/AuthController.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+var User = require('../user/User');
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
+var config = require('../config');
